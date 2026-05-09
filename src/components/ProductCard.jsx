5dd7eeb4e8ce58da46f81d0f128a4cd50741c468
mayangpuspitasariii <mayangpuspitasari0901@gmail.com>
2026-05-09 10:52:44 +0530

    perbaikan ui cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,36 +1,38 @@
 function ProductCard({ name, price, image, onAddToCart }) {
   return (
-    <div className="w-64 rounded-xl shadow-md p-4 hover:shadow-xl transition">
-      {/* Gamabr */}
+    <div className="w-64 rounded-xl shadow-md p-4 hover:shadow-xl transition duration-300">
+      {/* Gambar */}
       <img
         src={image}
         alt={name}
         className="w-full h-48 object-cover rounded-lg"
       />
 
-      {/* Nama Produk */}
+      {/* Nama */}
       <h2 className="text-xl font-bold mt-3">{name}</h2>
 
       {/* Harga */}
       <p className="text-gray-600 mt-1">Rp. {price}</p>
 
-      {/* Button */}
-      <div className="flex w-full justify-between gap-4">
-        <button className="w-full mt-4 bg-black text-white py-2 rounded-lg hover:opacity-80 cursor-pointer">
-          Beli Sekarang
+      {/* Button 2 kolom */}
+      <div className="flex gap-3 mt-4">
+        <button className="flex-1 bg-black text-white py-2 rounded-lg hover:opacity-80 cursor-pointer">
+          Beli
         </button>
+
         <button
           onClick={onAddToCart}
-          className="bg-amber-500 text-white rounded-lg hover:opacity-80 cursor pointer"
+          className="flex-1 bg-amber-500 text-white py-2 rounded-lg hover:opacity-80 cursor-pointer"
         >
-          Tambah ke Keranjang
+          Cart
         </button>
       </div>
-      <button className="w-full mt-4 bg-black text-white py-2 rounded-lg hover:opacity-80 cursor-pointer">
-        Lihat Detail Produk
+
+      {/* Detail */}
+      <button className="w-full mt-3 border py-2 rounded-lg hover:bg-gray-100 cursor-pointer">
+        Detail Produk
       </button>
     </div>
   );
 }
-
 export default ProductCard;
